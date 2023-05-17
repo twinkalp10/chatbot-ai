7099d78cd21a6adf38998b381f9ec2c73743f665
twinkal <twinkal.work@outlook.com>
2023-05-17 23:23:20 +0530

    logger package removed

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,9 +2,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import server from "./server";
-import { log } from "logger";
 const port = process.env.PORT || 5001;
 
 server.listen(port, () => {
-  log(`api running on ${port}`);
+  console.log(`api running on ${port}`);
 });
