2bfba674c3a9e7d13c5c49334bec10a17c906329
Pranav <pranavkp.me@outlook.com>
2023-05-20 14:00:50 +0530

    fix build

--- apps/web/app/dashboard/chatbot/page.tsx
+++ apps/web/app/dashboard/chatbot/page.tsx
@@ -1,47 +1,47 @@
 "use client"
 
 import React from "react"
-import { RefreshCcwIcon } from "lucide-react"
+import { RefreshCcw } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 const Page = () => {
   return (
-    <div className="w-96 rounded h-[38rem] flex flex-col justify-between overflow-auto border-zinc-200 border p-2">
-      <div className="flex space-y-5 flex-col">
+    <div className="flex h-[38rem] w-96 flex-col justify-between overflow-auto rounded border border-zinc-200 p-2">
+      <div className="flex flex-col space-y-5">
         <div className="self-end">
-          <RefreshCcwIcon />
+          <RefreshCcw />
         </div>
         <div className="flex flex-col space-y-2">
           <div className="flex justify-start">
-            <div className="rounded-lg bg-gray-200  text-black py-2 px-4">
+            <div className="rounded-lg bg-gray-200  px-4 py-2 text-black">
               <p className="text-sm ">Hello, how can I assist you today?</p>
             </div>
           </div>
           <div className="flex justify-end">
-            <div className="rounded-lg bg-blue-500 text-white py-2 px-4">
+            <div className="rounded-lg bg-blue-500 px-4 py-2 text-white">
               <p className="text-sm">I have a question about your products.</p>
             </div>
           </div>
           <div className="flex justify-start">
-            <div className="rounded-lg bg-gray-200  text-black py-2 px-4">
-              <p className="text-sm">Sure, I'd be happy to help!</p>
+            <div className="rounded-lg bg-gray-200  px-4 py-2 text-black">
+              <p className="text-sm">Sure, I&apos;d be happy to help!</p>
             </div>
           </div>
           <div className="flex justify-end">
-            <div className="rounded-lg bg-blue-500 text-white py-2 px-4">
+            <div className="rounded-lg bg-blue-500 px-4 py-2 text-white">
               <p className="text-sm">Thank you for your assistance.</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="flex w-full max-w-sm items-center space-x-2 p-2 border rounded-md">
+      <div className="flex w-full max-w-sm items-center space-x-2 rounded-md border p-2">
         <Input
           type="text"
           placeholder="Type your message..."
-          className="w-full border-none border-transparent focus:ring-0 focus:outline-none appearance-none"
+          className="w-full appearance-none border-none border-transparent focus:outline-none focus:ring-0"
         />
         <Button type="submit">Send</Button>
       </div>
--- apps/web/app/dashboard/content/website/page.tsx
+++ apps/web/app/dashboard/content/website/page.tsx
@@ -4,17 +4,13 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 
-type ChatbotInputProps = {
-  onSubmit: () => void
-}
-
-const Page = ({ onSubmit }: ChatbotInputProps) => {
+const Page = () => {
   return (
     <div className="max-w-2xl">
       <div className="flex flex-col items-start gap-3">
         <div>
           <h2 className="text-2xl font-semibold tracking-tight">Add Website</h2>
-          <p className="text-sm text-muted-foreground">Add Website URL.</p>
+          <p className="text-muted-foreground text-sm">Add Website URL.</p>
         </div>
         <div className="flex w-full max-w-2xl items-center space-x-2">
           <Input type="email" placeholder="Email" />
