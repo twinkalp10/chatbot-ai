43b228ba51762c78e1e984052077c6996b47f4d0
twinkal <twinkal.work@outlook.com>
2023-05-21 22:28:46 +0530

    selected chatbot on dashboard opens new page that shows chatbotId

--- apps/web/app/dashboard/[chatbotId]/page.tsx
+++ apps/web/app/dashboard/[chatbotId]/page.tsx
@@ -1,10 +1,11 @@
+"use client"
+
 import React from "react"
+import { useParams, usePathname } from "next/navigation"
 
-export function getStaticParams() {}
-
-const Chatbot = ({ params }: any) => {
-  const { chatbotId } = params
-  return <div>{chatbotId}</div>
+const Chatbot = () => {
+  const pathname = useParams()
+  return <div>{pathname.chatbotId}</div>
 }
 
 export default Chatbot
