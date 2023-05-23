c7bd10f5170ffc18bee29801b419b608b3bc0ae0
Pranav <pranavkp.me@outlook.com>
2023-05-24 00:29:48 +0530

    added logo

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -2,7 +2,9 @@
 
 import { ReactNode } from "react"
 import { Metadata } from "next"
+import Link from "next/link"
 import { useParams } from "next/navigation"
+import { MessageCircle } from "lucide-react"
 
 import ChatbotSwitcher from "./components/chatbotSwitcher"
 import { MainNav } from "./components/main-nav"
@@ -21,6 +23,13 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
                 : "flex h-16 items-center px-4 py-2"
             }
           >
+            <Link href="/dashboard" className="flex items-center gap-2">
+              <MessageCircle className="h-6 w-6" />
+
+              <p className="font-bold leading-tight tracking-tighter">
+                Chatbot AI
+              </p>
+            </Link>
             <div className="flex flex-col gap-3">
               <ChatbotSwitcher />
               <MainNav className="mx-2" />
