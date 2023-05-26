e0132ec0ce9aa797ded0a44931b2dc986ba38db3
Pranav <pranavkp.me@outlook.com>
2023-05-26 17:30:12 +0530

    fix the build

--- apps/web/app/settings/page.tsx
+++ apps/web/app/settings/page.tsx
@@ -1,19 +1,16 @@
 import { Separator } from "@/components/ui/separator"
 
-import { AccountForm } from "./account/page"
-
 export default function SettingsAccountPage() {
   return (
     <div className="space-y-4">
       <div>
         <h3 className="text-lg font-medium">Account</h3>
-        <p className="text-sm text-muted-foreground">
+        <p className="text-muted-foreground text-sm">
           Update your account settings. Set your preferred language and
           timezone.
         </p>
       </div>
       <Separator />
-      <AccountForm />
     </div>
   )
 }
